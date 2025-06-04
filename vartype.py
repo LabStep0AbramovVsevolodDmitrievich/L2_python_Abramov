18e4606e4f78f1356a817bef0e9c15dc84691b53
Абрамов Всеволод Дмитриевич <abramov.v.d@corp.kp11.ru>
2025-06-04 09:58:43 +0300

    2

---
+++ vartype.py
@@ -0,0 +1,13 @@
+# Задание 4
+num1 = float(input("Введите первое число: "))
+num2 = float(input("Введите второе число: "))
+print(f"Сумма: {num1 + num2}")
+
+# Задание 5
+number = input("Введите число: ")
+converted = int(number)
+print(f"Тип данных: {type(converted)}")
+
+# Задание 6
+age = input("Введите ваш возраст: ")
+print(f"Вам {age} лет.")
